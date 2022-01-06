5a657a8691520223ff35eebf5b85b3fbc8fc0c0a
marynapl <kushnirmarinka@gmail.com>
2022-01-05 19:33:57 -0800

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,5 +39,19 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [
+          'UA-175231750-1', // Google Analytics / GA
+        ],
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+        },
+      },
+    },
   ],
 }
